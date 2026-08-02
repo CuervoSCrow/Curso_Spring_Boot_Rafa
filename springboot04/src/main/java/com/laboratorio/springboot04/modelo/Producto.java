1c3b57509d0633a557c4a04291edb4f6eb0d6e53
Cuervo <vidalcuervo@gmail.com>
2026-08-02 17:22:38 -0600

    Capa Servicios

---
+++ springboot04/src/main/java/com/laboratorio/springboot04/modelo/Producto.java
@@ -0,0 +1,13 @@
+package com.laboratorio.springboot04.modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+public class Producto {
+    private Integer codigo;
+    private String nombre;
+    private Double precio;
+}
