9a8cc817ef9b2061d3c0f7478d93441a0168c79b
Cuervo <vidalcuervo@gmail.com>
2026-08-12 14:30:35 -0600

    Spring Data JPA, consultas derivadas

--- springboot14/src/main/java/com/laboratorio/springboot14/modelo/Producto.java
+++ springboot14/src/main/java/com/laboratorio/springboot14/modelo/Producto.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "productos")
 @Getter @Setter @RequiredArgsConstructor
@@ -12,8 +14,32 @@ public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer codigo;
+
+    @Column(name="categoria_id",nullable = false)
+    private Integer categoriaId;
+
     @Column(name = "nombre", nullable = false, length = 121)
     private String nombre;
+
     @Column(name = "precio", nullable = false)
     private double precio;
+
+    @Column(name="fecha_ingreso",nullable = false)
+    private LocalDate fechaIngreso;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="categoria_id",insertable = false,
+                updatable = false)
+    private Categoria categoria;
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "codigo=" + codigo +
+                ", categoriaId=" + categoriaId +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", fechaIngreso=" + fechaIngreso +
+                '}';
+    }
 }
