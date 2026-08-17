18738403024e6ae74f5674f8099d25c1fabc9dbb
Cuervo <vidalcuervo@gmail.com>
2026-08-17 03:54:54 -0600

    Spring Data JPA, consultas nativas

--- springboot16/src/main/java/com/laboratorio/springboot16/util/Runner.java
+++ springboot16/src/main/java/com/laboratorio/springboot16/util/Runner.java
@@ -1,4 +1,36 @@
 package com.laboratorio.springboot16.util;
 
-public class Runner {
+import com.laboratorio.springboot16.model.Producto;
+import com.laboratorio.springboot16.repository.ProductoRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@Slf4j
+public class Runner implements CommandLineRunner {
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        String nombre = "cable de red";
+        String buscar = "ble";
+
+        log.info("Ejecutando Runner");
+        Optional<Producto> productos;
+
+//        Buscar un registro por nombre exacto
+        productos = this.productoRepository.findOneByNombre(nombre);
+        if(productos.isEmpty()){
+            log.info("No se encontro el producto con nombre: " + nombre);
+        } else {
+            log.info("Producto encontrado: " + productos.get());
+        }
+
+
+    }
 }
