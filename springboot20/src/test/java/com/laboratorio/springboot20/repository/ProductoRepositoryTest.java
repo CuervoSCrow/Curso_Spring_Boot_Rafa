d21f630436e2cdacf9555b1812d6939666ef0606
Cuervo <vidalcuervo@gmail.com>
2026-08-24 17:31:30 -0600

    DTOs en la capa de servicios

---
+++ springboot20/src/test/java/com/laboratorio/springboot20/repository/ProductoRepositoryTest.java
@@ -0,0 +1,66 @@
+package com.laboratorio.springboot20.repository;
+
+import com.laboratorio.springboot20.dto.ProductoResponse;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class ProductoRepositoryTest {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @Test
+    void findProductoByIdTest(){
+        Integer id = 4;
+
+        ProductoResponse response = this.productoRepository.findProductoById(id).get();
+
+        Assertions.assertEquals(id,response.getCodigo());
+        Assertions.assertEquals(2,response.getCategoriaId());
+    }
+
+    @Test
+    void findProductoByNombreTest(){
+        String nombre = "Producto 7";
+
+        ProductoResponse response = this.productoRepository.findByProductoByNombre(nombre).get();
+
+        Assertions.assertEquals(nombre,response.getNombre());
+        Assertions.assertEquals(3,response.getCategoriaId());
+    }
+
+    @Test
+    void findAllOrderByNombreAscTest(){
+        List<ProductoResponse> productos =
+                this.productoRepository.findAllOrderByNombreAsc();
+
+        Assertions.assertEquals(9,productos.size());
+
+    }
+
+    @Test
+    void findByNombreContainingIgnoreCaseOrderByNombreAscTest(){
+        String infix = "oDUc";
+
+        List<ProductoResponse> productos =
+                this.productoRepository.findByNombreContainingIgnoreCaseOrderByNombreAsc(infix);
+
+        Assertions.assertEquals(9,productos.size());
+    }
+
+    @Test
+    void findByCategoriaIdOrderByNombreAsc(){
+        Integer categoriaId = 3;
+        List<ProductoResponse> productos =
+                this.productoRepository.findByCategoriaIdOrderByNombreAsc(categoriaId);
+
+        Assertions.assertEquals(3,productos.size());
+    }
+}
