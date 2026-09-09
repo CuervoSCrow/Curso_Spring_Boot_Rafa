f97769009f872271d2c200abbbe214154acbd0b9
CuervosCrow <vidalcuervo@gmail.com>
2026-09-09 01:09:20 -0600

    Pruebas unitarias de la capa de servicios

--- springboot21/src/test/java/com/laboratorio/springboot21/unit/service/ProductoServiceTest.java
+++ springboot21/src/test/java/com/laboratorio/springboot21/unit/service/ProductoServiceTest.java
@@ -185,5 +185,25 @@ public class ProductoServiceTest {
 
     }
 
+    @Test
+    void createProduct_ProductoExists(){
+        ProductoRequest request = new ProductoRequest(1,"Mouse",10.0);
+        ProductoResponse productoDB = new ProductoResponse(
+                1,
+                1,
+                "Mouse",
+                10.0,LocalDate.now());
+        when(this.productoRepository.findProductoByNombre(request.getNombre()))
+                .thenReturn(Optional.of(productoDB));
+
+        ProductoResponse producto = this.productoService.createProducto(request);
+        assertNotNull(producto);
+        assertEquals(1,producto.getCodigo());
+        assertEquals("Mouse",producto.getNombre());
+        verify(this.productoRepository).findProductoByNombre("Mouse");
+        verify(this.categoriaService, never()).findCategoriaById(1);
+        verify(this.productoRepository, never()).save(any(Producto.class));
+    }
+
 
 }
