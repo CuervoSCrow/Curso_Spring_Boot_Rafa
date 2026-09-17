d2d85554291b738d3f5aa573268039299dcc2a26
CuervosCrow <vidalcuervo@gmail.com>
2026-09-16 18:52:29 -0600

    Usando los dtos en la capa de los controladores - Iniciando model , dto

---
+++ springboot23/src/main/java/com/laboratorio/springboot23/Springboot23Application.java
@@ -0,0 +1,13 @@
+package com.laboratorio.springboot23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot23Application.class, args);
+	}
+
+}
---
+++ springboot23/src/main/java/com/laboratorio/springboot23/repository/ProductoRepository.java
@@ -0,0 +1,5 @@
+package com.laboratorio.springboot23.repository;
+
+public interface ProductoRepository {
+
+}
